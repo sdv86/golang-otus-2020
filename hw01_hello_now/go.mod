6c21dbf1d0f4de3b03e87f6a97198c3e7998918a
sdv <d.stolyarov2008@gmail.com>
2020-11-25 21:26:33 +0300

    HW1 is completed

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw01_hello_now
+module github.com/sdv86/golang-otus-2020/hw01_hello_now
 
 go 1.15
 
@@ -6,5 +6,5 @@ require (
 	bou.ke/monkey v1.0.2
 	github.com/beevik/ntp v0.2.0
 	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 )
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const Layout = "2006-01-02 15:04:05 -0700 MST"
+
+func Today() string {
+	return time.Now().Round(0).Format(Layout)
+}
+func TodayNtp() string {
+	ntpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("Fatal Error: %v\n", err)
+	}
+	return ntpTime.Round(0).Format(Layout)
+}
 func main() {
-	// Place your code here
+	fmt.Println("current time:", Today())
+	fmt.Println("exact time:", TodayNtp())
 }
